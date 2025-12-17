ace3ff142382a25316a0be81ec1ffadca2f122dc
Tun Htet Aung <tunhtetaung532@gmail.com>
2025-12-17 10:51:40 +0630

    Add middleware for Product

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'category_name' => $this->category->name,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'image' => asset('productImages/' . $this->image),
+            'status' => $this->status === 1 ? "Active" : "Expired",
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,25 @@ use Illuminate\Http\Request;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\CategoryController;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 
-// Route::get('/categories', [CategoryController::class, 'index']);
-// Route::get('/categories/{id}', [CategoryController::class, 'show']);
-
+//for category CRUD with authentication
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::post('/categories/{id}', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+});
+
+//for product CRUD with authentication
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::post('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
 });
